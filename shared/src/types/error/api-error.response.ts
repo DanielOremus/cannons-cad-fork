47eea4821f2a4c854ffaa01b50d4b2d0f52fc97c
Andrii Maltsev <maltsand@fit.cvut.cz>
2026-08-22 18:20:39 +0200

    change to the api response validation

--- shared/src/types/error/api-error.response.ts
+++ shared/src/types/error/api-error.response.ts
@@ -1,8 +1,21 @@
 import type { ValidationIssue } from './validation.error.js';
+import type { ErrorCode } from './error.code.ts';
 
 export type ApiErrorResponse = {
-  errorCode: string;
+  errorCode: ErrorCode;
   errorMessage?: string;
-  errorMsg?: string;
   errorIssues?: ValidationIssue[];
 };
+
+
+// export type ApiErrorResponse =
+//   | {
+//       errorCode: 'VALIDATION_FAILED';
+//       errorMessage?: string;
+//       errorIssues: ValidationIssue[];
+//     }
+//   | {
+//       errorCode: Exclude<ErrorCode, 'VALIDATION_FAILED'>;
+//       errorMessage?: string;
+//       errorIssues?: never;
+//     };
