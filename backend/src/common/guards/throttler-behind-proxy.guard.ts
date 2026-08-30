52018bbd7e8a3af2fb629c34c52bc21863909fe9
DanielOremus <oremusd89@gmai.com>
2026-08-30 22:06:18 +0300

    fix: eslint errors

--- backend/src/common/guards/throttler-behind-proxy.guard.ts
+++ backend/src/common/guards/throttler-behind-proxy.guard.ts
@@ -2,11 +2,12 @@ import { ThrottlerGuard, ThrottlerLimitDetail } from '@nestjs/throttler';
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AppError } from '../../shared/errors/app.error';
 import { ErrorCode } from '@project/shared';
+import { Request } from 'express';
 
 @Injectable()
 export class ThrottlerBehindProxyGuard extends ThrottlerGuard {
-  protected async getTracker(req: Record<string, any>): Promise<string> {
-    return req.ips.length ? req.ips[0] : req.ip;
+  protected async getTracker(req: Request): Promise<string> {
+    return await Promise.resolve(req.ips.length ? req.ips[0] : (req.ip ?? '127.0.0.1'));
   }
   protected throwThrottlingException(
     context: ExecutionContext,
